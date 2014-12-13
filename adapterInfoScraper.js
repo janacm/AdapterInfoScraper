24ce3279adfe25d6d691a4701fde0ccf6629ce6c
janac meena <janac.meena@gmail.com>
2014-12-13 09:18:10 -0500

    manager assigned me a different project
    Just committing the code I last wrote so I can pick up where I started
    at another time

--- adapterInfoScraper.js
+++ adapterInfoScraper.js
@@ -10,13 +10,21 @@ console.log("loaded");
         - output as CSV
 **/
 
+/*
+    Runs regex on buildtags 
+    ex. on "<html><body><h1>/builds/CCartifacts/release8-ADAPTERS/adapter8-0-1-USP-acme-ems-nnc8</h1><table border='black' borderwidth='1' cellpadding='5'><tr><th>name</th><th>file size</th><th>modified date</th></tr>
+    <tr><td><a href="/cruisecontrol/artifacts/release8-ADAPTERS//adapter8-0-1-USP-acme-ems-nnc8/acme-ems-nnc8-R8_0_1-DEV_USP-1-0-0-1">acme-ems-nnc8-R8_0_1-DEV_USP-1-0-0-1/</a></td><td align='right'>4K</td><td align='right'>Tue May 08 05:00:06 EDT 2012</td></tr></table></body></html>"
 
+    run /(DEV([aA-zZ]|-|[0-9])*)/g; 
+
+    get: 
+*/
 function getAllDevTags(page){
     var tags = new Array();
     var tagsWithoutLetters = new Array();
     // var re = /(DEV(-|[0-9])*)/g;
     var re = /(DEV([aA-zZ]|-|[0-9])*)/g; //catches EMS
-
+    
     tags = page.match(re);
 
     if (tags == null){
@@ -77,8 +85,9 @@ function getFullBuildTag(currentString){
 }
 
 function extract(){
-  // numOfLinks = document.getElementsByTagName('td').length;
+  numOfLinks = document.getElementsByTagName('a').length;
   // for (var i = 0; i < numOfLinks; i++){
+    // alert(numOfLinks);
     var links = new Array();
     var allDevTags = new Array();
     var latestDevTag = "";
@@ -87,19 +96,66 @@ function extract(){
     var currentString = "";
     var myTest; 
 
-    for (var i = 0; i < 20; i++){
+    // for (var i = 0; i < 20; i++){
+    for (var i = 0; i < numOfLinks; i++){
         links[i] = document.getElementsByTagName("a")[i].href;
         currentString = links[i].toString();
 
         if  ((currentString.search("adapter") > 0) &&
             (currentString.search("INHOUSE") < 0) &&
             (currentString.search("v099") < 0)){
+            $.get(currentString, parseResponse(responseText));
                 
-            console.log("currentString: " + currentString);
-            console.log("build tag: " + getFullBuildTag(currentString));
-            $.get(currentString, function(responseText){
-                console.log("latest dev tag:" + getLatestDevTag(getAllDevTags(responseText)));
+                function() parseResponse(){
+            // console.log("currentString: " + currentString);
+            // console.log("build tag: " + getFullBuildTag(currentString));
+            /*$.get(currentString, function(responseText){
+                // console.log("latest dev tag:" + getLatestDevTag(getAllDevTags(responseText)));
+/*                // $('#adapterInfoTable tbody').after('<tr>');
+            // Customer
+                // Name
+                $('#adapterInfoTable tr:last').after('<td>' + "ATT" + '</td>');
+                // Organization
+                $('#adapterInfoTable tr:last').after('<td>' + "USP" + '</td>');
+            
+            // Adapter
+                // Name
+
+                $('#adapterInfoTable tr:last').after('<td>' + "Hitachi-SuperJEngineFramework-v2-R8" + '</td>');
+                // // Type
+                $('#adapterInfoTable tr:last').after('<td>' + "EMS" + '</td>');
+                // // Functionality
+                $('#adapterInfoTable tr:last').after('<td>' + "All" + '</td>');
+            
+            // Nakina
+                // Release
+                $('#adapterInfoTable tr:last').after('<td>' + "rel" + '</td>');
+            
+            // NE
+                // Vendor
+                $('#adapterInfoTable tr:last').after('<td>' + "Vendor" + '</td>');
+                // Model
+                $('#adapterInfoTable tr:last').after('<td>' + "Model" + '</td>');
+                // Version
+                $('#adapterInfoTable tr:last').after('<td>' + "Version" + '</td>');
+            
+*/            // Last DEV Build Tag
+                // Tag
                 $('#adapterInfoTable tr:last').after('<tr><td>' + getLatestDevTag(getAllDevTags(responseText)) + '</td></tr>');
+/*                $('#adapterInfoTable tr:last').after('<td>' + "latestDevTag" + '</td>');
+                // When
+                $('#adapterInfoTable tr:last').after('<td>' + "tuesday" + '</td>');
+            
+            // Last CA Build Tag
+                // Tag
+                $('#adapterInfoTable tr:last').after('<td>' + "latestCAtag"+ '</td>');
+                // When
+                $('#adapterInfoTable tr:last').after('<td>' + "sunday"+ '</td>');
+            // Explanation and additional notes
+                $('#adapterInfoTable tr:last').after('<td>' + "fragile"+ '</td>');
+
+                // Last DEV Build tag
+        */    // $('#adapterInfoTable tr:last').after('</tr>');
             });
         }
     }
